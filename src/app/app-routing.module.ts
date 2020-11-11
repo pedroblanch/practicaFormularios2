3ad0929e2428e5724a4128d91ac90e2a8e75e6d5
Pedro Blanch <pedroblanch@iesjulioverne.es>
2020-11-11 21:24:37 +0100

    Version inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'pagina2',
+    loadChildren: () => import('./pagina2/pagina2.module').then( m => m.Pagina2PageModule)
+  },
 ];
 
 @NgModule({
